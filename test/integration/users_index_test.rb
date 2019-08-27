e065266c95cab7d3cbfd60d7479e4a712a16c0b2
Asger Bjerre-Nielsen <sirjazzper@gmail.com>
2019-08-27 16:03:55 +0200

    Added mailer functionality

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -28,4 +28,21 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
+
+  test "should only show profiles of activated users" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), false
+      end
+    end
+  end
 end
